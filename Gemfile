6e5cebb28774faf41454c970bbbdea9297a81df5
B V Satyaram <bvsatyaram@gmail.com>
2015-10-09 13:42:32 +0530

    Add validations to tasks

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ end
 group :development do
   gem 'web-console', '~> 2.0'
   gem 'spring'
+  gem 'annotate'
 end
 
 group :production do
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  done        :boolean          default(FALSE)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
